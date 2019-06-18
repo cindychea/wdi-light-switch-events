107dd1d8f9ca075c799d9c887355b6fd64961abc
cindychea <cindy_chea@hotmail.com>
2019-06-18 14:26:16 -0400

    completed light switch exercise

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
+const background = document.querySelector('body');
+const button = document.querySelector('button');
+const status = document.querySelector('.status');
 
+function lightSwitch() {
+    if (button.classList.contains('on') && status.firstChild.nodeValue === "It's so bright in here!") {
+        button.classList.remove('on');
+        button.classList.add('off');
+        status.firstChild.nodeValue = "Hey, who turned off the lights?";
+        background.style.backgroundColor = '#000';
+        status.style.color = '#FFF';
+    } else if (button.classList.contains('off') && status.firstChild.nodeValue === "Hey, who turned off the lights?") {
+        button.classList.remove('off');
+        button.classList.add('on');
+        status.firstChild.nodeValue = "It's so bright in here!";
+        background.style.backgroundColor = '#FFF';
+        status.style.color = '#000';
+    }
+};
 
+button.addEventListener('click', lightSwitch);
